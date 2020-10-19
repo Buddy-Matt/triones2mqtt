cbf42f3220f4fac4d8495e773ab270a9261e2dec
Matt Gray <Matt@Matt-Gray.co.uk>
2020-10-20 00:38:05 +0100

    Add disconnect handler, improve print

--- triones2mqtt.py
+++ triones2mqtt.py
@@ -7,13 +7,74 @@ import yaml
 import paho.mqtt.client as mqtt
 
 
+def log(msg):
+  print(msg)
+  sys.stdout.flush()
+
+def sendState():
+  if state["state"] == "ON":
+    msg = json.dumps(state)    
+  else:
+    msg = json.dumps({"state":state["state"]})
+    
+  log("Sending state " + msg)
+
+  client.publish(state_topic,msg)
+
+def writeRGB(_rgb):
+  state["color"] = _rgb
+  state["white_value"] = 0
+  device.char_write_handle(7, bytearray([0x56,_rgb["r"],_rgb["g"],_rgb["b"],0,0xF0,0xAA]))
+  sendState()
+
+def writeWhite(_wv):
+  state["color"] = {"r":0,"g":0,"b":0}
+  state["white_value"] = _wv
+  device.char_write_handle(7, bytearray([0x56,0,0,0,_wv,0x0F,0xAA]))
+  sendState()
+
+def writeOff():
+  state["state"] = "OFF"
+  device.char_write_handle(7, bytearray([0xCC, 0x24, 0x33]))
+  sendState()
+
+def writeOn():
+  state["state"] = "ON"
+  device.char_write_handle(7, bytearray([0xCC, 0x23, 0x33]))
+  sendState()
+  
+def mqtt_message(client, userdasta, message):
+  msg = str(message.payload.decode("utf-8"))
+  log("Received SET " + msg)
+  data = json.loads(msg)
+  
+  if "color" in data:
+    rgb = data["color"]
+    writeRGB(rgb)
+  if "white_value" in data:
+    writeWhite(data["white_value"])
+  if "state" in data:
+    if data["state"] == "ON":
+      writeOn()
+    elif data["state"] == "OFF":
+      writeOff()
+    elif data["state"] == "TOGGLE":
+      if state["state"] == "ON":
+        writeOff()
+      else:
+        writeOn()
+
+def mqtt_disconnect(client, userdata, rc):
+    if rc != 0:
+        log ("MQTT unexpected disconnect. Reconnecting...")
+
+
 ####Read Config
 with open("config.yaml","r") as stream:
   try:
     config = yaml.load(stream, Loader=yaml.SafeLoader)
   except:
-    print("malformed config file")
-    sys.stdout.flush()
+    log("malformed config file")
     sys.exit()
 
 
@@ -55,92 +116,32 @@ state = {
   "white_value": 0
 }
 
-def sendState():
-  if state["state"] == "ON":
-    msg = json.dumps(state)    
-  else:
-    msg = json.dumps({"state":state["state"]})
-    
-  print("Sending state " + msg)
-  sys.stdout.flush()
-
-  client.publish(state_topic,msg)
-
-def writeRGB(_rgb):
-  state["color"] = _rgb
-  state["white_value"] = 0
-  device.char_write_handle(7, bytearray([0x56,_rgb["r"],_rgb["g"],_rgb["b"],0,0xF0,0xAA]))
-  sendState()
-
-def writeWhite(_wv):
-  state["color"] = {"r":0,"g":0,"b":0}
-  state["white_value"] = _wv
-  device.char_write_handle(7, bytearray([0x56,0,0,0,_wv,0x0F,0xAA]))
-  sendState()
-
-def writeOff():
-  state["state"] = "OFF"
-  device.char_write_handle(7, bytearray([0xCC, 0x24, 0x33]))
-  sendState()
-
-def writeOn():
-  state["state"] = "ON"
-  device.char_write_handle(7, bytearray([0xCC, 0x23, 0x33]))
-  sendState()
-  
-def mqtt_message(client, userdasta, message):
-  msg = str(message.payload.decode("utf-8"))
-  print("Received SET " + msg)
-  sys.stdout.flush()
-  data = json.loads(msg)
-  
-  if "color" in data:
-    rgb = data["color"]
-    writeRGB(rgb)
-  if "white_value" in data:
-    writeWhite(data["white_value"])
-  if "state" in data:
-    if data["state"] == "ON":
-      writeOn()
-    elif data["state"] == "OFF":
-      writeOff()
-    elif data["state"] == "TOGGLE":
-      if state["state"] == "ON":
-        writeOff()
-      else:
-        writeOn()
-
-
 adapter.start()
 device = None
 
 while device is None:
   try:
-    print("Connecting to lightbulb")
-    sys.stdout.flush()
+    log("Connecting to lightbulb")
     device = adapter.connect(address)
   except:
-    print ("Error connecting")
-    sys.stdout.flush()
+    log ("Error connecting")
   else:
-    print ("Lightbulb connected")
-    sys.stdout.flush()
+    log ("Lightbulb connected")
 
 try:
-  print ("Connecting to MQTT")
-  sys.stdout.flush()
+  log ("Connecting to MQTT")
   client = mqtt.Client()
   client.will_set(availability_topic, "offline", 0, True)
   if config["username"] != None and config ["password"] != None:
     client.username_pw_set(config["username"],config["password"])
   client.connect(config["server"])
-  print ("MQTT Connected")
-  sys.stdout.flush()
+  log ("MQTT Connected")
   client.subscribe(command_topic)
   client.publish(availability_topic, "online", 0, True)
   if config["homeassistant"] != None:
     client.publish("homeassistant/light/" + config["homeassistant"].replace(" ","").lower() + "/light/config",json.dumps(haConfig),0,True)
   client.on_message=mqtt_message
+  client.on_disconnect=mqtt_disconnect
   client.loop_forever()
 finally:
   adapter.stop()
